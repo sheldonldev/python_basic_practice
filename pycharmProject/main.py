54c6215281f9cf4da7d34a8de8eb836e7d033d1f
sheldonldev <xiadanli0320@gmail.com>
2021-07-28 21:25:49 +0800

    default

---
+++ pycharmProject/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    x = 5
+    y = 'string'
+
+    print(type(x))
+    print(type(y))
+
+    help(x)
+    help(y)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
